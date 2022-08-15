9f45a2b066d40d7b8afd6b1c4d06c2ac5fded916
liu <tree5130@126.com>
2022-08-15 14:11:09 +0800

    Music

---
+++ app/src/main/java/com/example/music/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.music;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer brid;
+    MediaPlayer cat;
+    MediaPlayer dog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        brid = MediaPlayer.create(this, R.raw.brid);
+        cat = MediaPlayer.create(this, R.raw.cat);
+        dog = MediaPlayer.create(this, R.raw.dog);
+    }
+
+
+    public void cat(View view) {
+        cat.start();
+    }
+
+    public void brid(View view) {
+        brid.start();
+    }
+
+    public void dog(View view) {
+        dog.start();
+    }
+}
